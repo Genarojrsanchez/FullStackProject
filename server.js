8987214ca4d11d035857ac2a1d4fdec2a59658b5
Genaro Sanchez <genarorwc@yahoo.com>
2020-03-11 13:34:27 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const dbupdateobject = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+};
+
+mongoose.connect(process.env.DATABASE_URL, dbupdateobject);
+
+
+db.on("error", (err) => (console.log(err.message + "is mongod not running?")));
+db.on("connected", () => (console.log("mongo connected: to the URL")));
+db.on("disconnected", () => (console.log("mongo disconnected")));
+
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log("=============================");
+  console.log(`listening on port: ${process.env.PORT}`);
+  console.log("=============================");
+})
