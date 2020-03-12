dc9c8f63f9afe883801826c7d8dd98833838796c
Genaro Sanchez <genarorwc@yahoo.com>
2020-03-12 10:03:22 -0700

    initial new route started 1 down

--- server.js
+++ server.js
@@ -13,19 +13,40 @@ mongoose.connect(process.env.DATABASE_URL, dbupdateobject);
 
 
 
+// ==============
+// new route to get data back sent to new.ejs file.
+// ==============
+
+app.get("/home/new", (req, response) => {
+  response.render("new.ejs")
+})
+// ==============
+//
+// ==============
+
 app.get("/",(req, response) => {
   response.send("it\'s working");
 })
 
 
-
-
-
-
-db.on("error", (err) => (console.log(err.message + "is mongod not running?")));
-db.on("connected", () => (console.log("mongo connected: to the URL")));
-db.on("disconnected", () => (console.log("mongo disconnected")));
-
+// // ==============
+// //
+// // ==============
+//
+// app.get("/variable/show", (req, response) => {
+//   response.send("my show page")
+// })
+//
+// // ==============
+// //
+// // ==============
+// app.get("/create/:id", (req, response) => {
+//   response.send("my create page")
+// })
+//
+// // ==============
+// // adjusting data delete edit and update
+// // ==============
 
 
 
@@ -34,3 +55,9 @@ app.listen(process.env.PORT, () => {
   console.log(`listening on port: ${process.env.PORT}`);
   console.log("=============================");
 })
+
+
+
+db.on("error", (err) => (console.log(err.message + "is mongod not running?")));
+db.on("connected", () => (console.log("mongo connected: to the URL")));
+db.on("disconnected", () => (console.log("mongo disconnected")));
